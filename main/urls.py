f81b47c798e21256f1aaf65bd417cb91c06de779
shivanidhyani <shivanidhyani07@gmail.com>
2020-06-15 01:24:53 +0530

    first commit

---
+++ main/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about_us', views.about, name='about'),
+    path('our_causes', views.causes, name='causes'),
+    path('our_blogs', views.blog, name='blog'),
+    path('contact_us', views.contact, name='contact'),
+    path('login_to_nsss', views.login, name='login'),
+    path('register_to_nsss', views.register, name='register'),
+    ]
---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def causes(request):
+    return render(request,'causes.html')
+def blog(request):
+    return render(request,'blog.html')
+def contact(request):
+	return render(request,'contact.html')
+def login(request):
+	return render(request,'login.html')
+def register(request):
+	return render(request,'register.html')
